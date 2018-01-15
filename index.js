9e2affef9913462b37d1597c8e5e8180d4e66d77
Michelle Ranly <meranly8@gmail.com>
2018-01-15 20:09:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,3 @@ var recipes = { }
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, obj, {[key]: value})
 }
-var obj = {prop: 1}
-updateObjectWithKeyAndValue(recipes, 'prop2', 2)
-recipes
